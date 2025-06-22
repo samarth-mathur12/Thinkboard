7576f1465ad92a84843420d80d5193b0b4e1d894
samarth-mathur12 <samarthmathur199@gmail.com>
2025-06-22 20:14:24 +0530

    Setting up Database i.e, MongoDB

--- Backend/src/controllers/notesController.js
+++ Backend/src/controllers/notesController.js
@@ -1,15 +1,15 @@
-export function getAllNotes(res, req){
-    res.status(200).send("you got fetched notes");
+export function getAllNotes(req, res) {
+    res.status(200).send("You got fetched notes");
 }
 
-export function createNotes(res, req){
-    res.status(201).json({message : "post created successfully!"});
+export function createNotes(req, res) {
+    res.status(201).json({ message: "Post created successfully!" });
 }
 
-export function updateNotes(res, req){
-    res.status(201).json({message: "Post updated successfully !"});
+export function updateNotes(req, res) {
+    res.status(201).json({ message: "Post updated successfully!" });
 }
 
-export function deleteNotes(res, req){
-    res.status(201).json({message: "Post deleted successfully !"});
+export function deleteNotes(req, res) {
+    res.status(201).json({ message: "Post deleted successfully!" });
 }
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import notesRoutes from "../src/routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 
 const app = express();
+const PORT = process.env.PORT || 5001
+
+connectDB();
 
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () =>{
-    console.log("Server started on PORT: 5001");
+app.listen(PORT, () =>{
+    console.log("Server started on PORT:", PORT);
 });
+
+// qANmxkXVoBzVjTlN
+
+//mongodb+srv://samarthmathur199:qANmxkXVoBzVjTlN@cluster0.d4wcqiw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
